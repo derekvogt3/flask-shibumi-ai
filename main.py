69a4fee9b01371214df49b9e3c0031681bd6b40c
Derek Vogt <derekvogt3@gmail.com>
2023-08-23 21:24:10 -0400

    testing

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
+
+from flask import Flask, jsonify, request, redirect, url_for
+from flask_cors import CORS
+from flask import Blueprint, request, jsonify
+from langchain.embeddings import OpenAIEmbeddings
+import os
+import pinecone
+from dotenv import load_dotenv
+from langchain.vectorstores import Pinecone
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import RetrievalQA
+
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def index():
-  return render_template('index.html')
+    data = request.json
+    query = data.get('query')
+    if not query:
+        return jsonify({"error": "Query parameter is missing from request body"}), 400
+    vectorstore.similarity_search(query=query, k=3)
+    llm = ChatOpenAI(openai_api_key=os.environ.get('OPENAI_API_KEY'),model_name='gpt-3.5-turbo', temperature=0.0)
+    qa = RetrievalQA.from_chain_type(llm=llm, chain_type='stuff', retriever=vectorstore.as_retriever())
+    return jsonify({"message": qa.run(data)}), 200
 
 if __name__ == '__main__':
   app.run(port=5000)
